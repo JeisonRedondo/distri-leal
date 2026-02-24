455f1ffaedc8f67028b475ba15371e2d00a6ea1e
Jeison Redondo <jeida16@yahoo.es>
2026-02-24 17:23:24 -0500

    [Add]Adding mockup presentational
    Generate a mockup for a brief presentation of how i hope this landing
    could to see

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,74 @@ import './App.css'
 
 function App() {
   return (
-    <h1>Actualizacion de pagina</h1>
+    <>
+      <nav>
+        <figure>logo</figure>
+        <section>searched input</section>
+        <ul>Options</ul>
+      </nav>
+      <main>
+        <h2>Products Off</h2>
+        <section>
+          <article>
+            Stelar Products
+          </article>
+          <article>
+            <article>
+              <article>
+                Stelar Products
+              </article>
+              Stelar Products
+            </article>
+            Stelar Products
+          </article>
+        </section>
+        <h2>Categories</h2>
+        <section>
+          <h2>Categorie n#</h2>
+          <div>
+            <article>
+              <p>image</p>
+              <h3>eslogan</h3>
+            </article>
+            <article>
+              <p>image</p>
+              <h3>eslogan</h3>
+            </article>
+            <article>
+              <p>image</p>
+              <h3>eslogan</h3>
+            </article>
+            <article>
+              <p>image</p>
+              <h3>eslogan</h3>
+            </article>
+          </div>
+          <h2>Categorie n#</h2>
+          <div>
+            <article>
+              <p>image</p>
+              <h3>eslogan</h3>
+            </article>
+            <article>
+              <p>image</p>
+              <h3>eslogan</h3>
+            </article>
+            <article>
+              <p>image</p>
+              <h3>eslogan</h3>
+            </article>
+            <article>
+              <p>image</p>
+              <h3>eslogan</h3>
+            </article>
+          </div>
+        </section>
+      </main>
+      <footer>
+        This is the footer
+      </footer>
+    </>
   )
 }
 
