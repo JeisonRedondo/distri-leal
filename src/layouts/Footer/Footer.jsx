669c3ec748df28ca8a3c6c3c806dca8ddbf097d1
Jeison Redondo <jeida16@yahoo.es>
2026-03-16 19:16:04 -0500

    [Add] Adding styles for footer
    Added some changes for the footer and styles

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -2,9 +2,19 @@ import "./Footer.css"
 function Footer() {
   return (
     <footer className="Footer-section">
-      <p>
-        This is the footer
-      </p>
+      <div><img className="footer-logo-img" src="../../../public/distri-leal-logo.png" alt=""></img></div>
+      <div className="footer-links">
+        <a href="">Products</a>
+        <a href="">About Us</a>
+        <a href="">Contact Us</a></div>
+      <div className="footer-contact">
+        <p>contacto@distrileal.com</p>
+        <p>Medellín, Colombia</p>
+      </div>
+      <div className="footer-github"><a href="">GitHub <span>Jeison Redondo</span></a></div>
+      <div className="footer-copyright">
+        <p>© 2026 Distri-Leal</p>
+      </div>
     </footer>
   )
 }
