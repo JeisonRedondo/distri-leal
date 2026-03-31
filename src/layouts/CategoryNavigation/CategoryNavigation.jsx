9857d2fcd971e64382f8f2614c9dba6a5bc31d4b
Jeison Redondo <jeida16@yahoo.es>
2026-03-30 19:11:11 -0500

    [Add]Function of Categories in CategoryNavigation
    Added function of reduce for categories on CategoryNavigation

--- src/layouts/CategoryNavigation/CategoryNavigation.jsx
+++ src/layouts/CategoryNavigation/CategoryNavigation.jsx
@@ -1,13 +1,33 @@
-import "./CategoryNavigation.css"
+import "./CategoryNavigation.css";
+import { products } from "../../data/products.js";
 
 function CategoryNavigation() {
+
+  const categories = products.reduce((categories, item) => {
+
+    if (!categories.includes(item.categoria)) {
+      categories.push(item.categoria)
+    }
+    return categories;
+
+  }, [])
+  console.log("CategoryNavigation - categories :", categories)
   return (
     <>
+
       <ul className="category-navigation__list flex-row">
-        <li className="category-navigation__item"><a className="category-navigation__link" href="">Tradicional</a></li>
-        <li className="category-navigation__item"><a className="category-navigation__link" href="">Snacks</a></li>
-        <li className="category-navigation__item"><a className="category-navigation__link" href="">Dulces</a></li>
-        <li className="category-navigation__item"><a className="category-navigation__link">Bebidas</a></li>
+        {
+          categories.map((cat) => (
+
+            <li className="category-navigation__item">
+              <a className="category-navigation__link" href="">
+                {
+                  cat.charAt(0).toUpperCase() + cat.slice(1)
+                }
+              </a>
+            </li>
+          ))
+        }
       </ul>
     </>
   )
